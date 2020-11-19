860f734816aef8861b48810a6990d81d5fc423bb
DXT <dangxuanthangqt@gmail.com>
2020-11-20 06:03:36 +0700

    release

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import theme from "./theme/index";
+import Routes from "./Routes";
+import "typeface-montserrat";
+import Alert from "./components/Alert/Alert";
+import "./assets/scss/index.scss";
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Alert></Alert>
+        <CssBaseline />
+        <Routes />
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/constants/Config.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://sun-booking-ticket.herokuapp.com/";
+//export const BASE_URL = "http://localhost:8080/";
